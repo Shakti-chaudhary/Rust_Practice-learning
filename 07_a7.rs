9d1c34733ab37d5222cc0aad047f518f3d450f30
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-02-15 16:59:37 +0530

    Day 2

---
+++ 07_a7.rs
@@ -0,0 +1,22 @@
+enum Color {
+    White,
+    Red,
+    Blue,
+    Pink,
+    Yellow,
+    Green,
+}
+fn print_color(go: Color) {
+    match go {
+        Color::White => println!("White color"),
+        Color::Red => println!("Red color"),
+        Color::Blue => println!("Blue color"),
+        Color::Pink => println!("Pink color"),
+        Color::Yellow => println!("Yellow color"),
+        Color::Green => println!("Green color"),
+    }
+}
+fn main() {
+    let color = Color::Red;
+    print_color(color);
+}
