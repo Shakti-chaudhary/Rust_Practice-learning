c2d683453c49b83ad803c93664c7f3d2cdf41c8e
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-02-15 15:10:01 +0530

    Day 2

---
+++ 04_4b.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let my_number = 54;
+
+    match my_number {
+        1 => println!("the value of a variable is 1"),
+        2 => println!("the value of a variable is 2"),
+        3 => println!("the value of a variable is 3"),
+        _ => println!("some other number"),
+    }
+}
