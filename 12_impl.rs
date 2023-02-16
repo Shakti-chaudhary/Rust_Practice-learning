de792bb90e038a2f4a1026e600dbb291fe468af7
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-02-16 14:47:30 +0530

    Day 3

--- 12_impl.rs
+++ 12_impl.rs
@@ -2,13 +2,26 @@ struct Temperature {
     degree_f: f64,
 }
 impl Temperature {
-    fn show_temp(temp: Temperature) {
+    fn freezing() -> Self {
+        Self { degree_f: 32.0 }
+    }
+    fn boiling() -> Self {
+        Self { degree_f: 212.0 }
+    }
+
+    fn show_temp(&self) {
         {
-            println!("temp = {:?}", temp.degree_f);
+            println!("temp = {:?}", self.degree_f);
         }
     }
 }
 fn main() {
     let hot = Temperature { degree_f: 99.9 };
-    Temperature::show_temp(hot)
+    hot.show_temp();
+
+    let cold = Temperature::freezing();
+    cold.show_temp();
+
+    let boil = Temperature::boiling();
+    boil.show_temp()
 }
