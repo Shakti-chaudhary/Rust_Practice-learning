03b2d913cc553118075dc5143b08550d3eb7d19e
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-02-17 21:28:40 +0530

    Day 4

---
+++ 18_documantetion.rs
@@ -0,0 +1,18 @@
+/// A favorite color.
+enum Color {
+    Red,
+    Blue,
+}
+
+/// A piece of mail.
+struct Mail {
+    /// The destination address.
+    address: String,
+}
+
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+// use cmd in terminal: cargo doc --open
+fn main() {}
