0220d341f282cb10dbf89be030e57682d74968d1
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-02-19 01:21:16 +0530

    Day 6

---
+++ 29_modules.rs
@@ -0,0 +1,27 @@
+use std::collections::HashMap;
+
+mod greet {
+    use std::collections::HashMap;
+    pub fn hello() {
+        println!("hello");
+    }
+    pub fn goodbye() {
+        println!("goodbye");
+    }
+}
+
+mod math {
+    pub fn add(a: i32, b: i32) -> i32 {
+        a + b
+    }
+    fn sub(a: i32, b: i32) -> i32 {
+        1
+    }
+}
+
+fn main() {
+    use greet::hello;
+    hello();
+    greet::goodbye();
+    math::add(1, 1);
+}
