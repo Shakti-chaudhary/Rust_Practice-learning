251ceef8bcfc404c78d65711d077f1463b3f4200
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-02-19 00:38:39 +0530

    Day 6

---
+++ 27_if_let.rs
@@ -0,0 +1,27 @@
+enum Color {
+    Red,
+    Blue,
+    Green,
+}
+fn main() {
+    // let maybe_user = Some("Jerry");
+    let maybe_user: Option<String> = None;
+    // match maybe_user {
+    //     Some(user) => println!("user = {:?}", user),
+    //     None => println!("no user"),
+    // }
+
+    if let Some(user) = maybe_user {
+        println!("user = {:?}", user);
+    } else {
+        println!("no user");
+    }
+
+    let red = Color::Red;
+
+    if let Color::Red = red {
+        println!("its red");
+    } else {
+        println!("its not red");
+    }
+}
