deb90bebe70b5d6473240b454ff349f5d89f78d6
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-02-20 12:03:32 +0530

    Day 7 stage 2

--- 33_project_one.rs
+++ 33_project_one.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io;
 fn get_input() -> Option<String> {
     let mut buffer = String::new();
@@ -36,20 +37,26 @@ pub struct Bill {
     amount: f64,
 }
 pub struct Bills {
-    inner: Vec<Bill>,
+    inner: HashMap<String, Bill>,
 }
 
 impl Bills {
     fn new() -> Self {
-        Self { inner: vec![] }
+        Self {
+            inner: HashMap::new(),
+        }
     }
 
     fn add(&mut self, bill: Bill) {
-        self.inner.push(bill);
+        self.inner.insert(bill.name.to_string(), bill);
     }
 
     fn get_all(&self) -> Vec<&Bill> {
-        self.inner.iter().collect()
+        self.inner.values().collect()
+    }
+
+    fn remove(&mut self, name: &str) -> bool {
+        self.inner.remove(name).is_some()
     }
 }
 mod menu {
@@ -69,6 +76,22 @@ mod menu {
         println!("Bill added");
     }
 
+    pub fn remove_bill(bills: &mut Bills) {
+        for bill in bills.get_all() {
+            println!("{:?}", bill);
+            println!("Enter bill name to remove:");
+        }
+        let name = match get_input() {
+            Some(name) => name,
+            None => return,
+        };
+        if bills.remove(&name) {
+            println!("bill removed");
+        } else {
+            println!("bill not found");
+        }
+    }
+
     pub fn view_bills(bills: &Bills) {
         for bill in bills.get_all() {
             println!("{:?}", bill);
@@ -79,6 +102,7 @@ mod menu {
 enum MainMenu {
     AddBill,
     ViewBill,
+    RemoveBill,
 }
 
 impl MainMenu {
@@ -86,6 +110,7 @@ impl MainMenu {
         match input {
             "1" => Some(Self::AddBill),
             "2" => Some(Self::ViewBill),
+            "3" => Some(Self::RemoveBill),
             _ => None,
         }
     }
@@ -93,7 +118,8 @@ impl MainMenu {
         println!("");
         println!(" == Bill Manager == ");
         println!("1. Add Bill");
-        println!("2. View Bill");
+        println!("2. View Bills");
+        println!("3. Remove Bill");
         println!("");
         println!("Enter selection: ");
     }
@@ -109,6 +135,7 @@ fn main() {
         match MainMenu::from_str(input.as_str()) {
             Some(MainMenu::AddBill) => menu::add_bill(&mut bills),
             Some(MainMenu::ViewBill) => menu::view_bills(&bills),
+            Some(MainMenu::RemoveBill) => menu::remove_bill(&mut bills),
             None => return,
         }
     }
