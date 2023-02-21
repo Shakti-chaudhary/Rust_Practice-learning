24c418f3b2659e2fb5725c328ff3a218d47a343c
Shakti Chaudhary <shaktichaudhary4321@gmail.com>
2023-02-22 00:50:11 +0530

    Day 9

---
+++ 39_39a.rs
@@ -0,0 +1,29 @@
+const MOCK_DATA: &'static str = include_str!("mock-data.csv");
+
+struct Names<'a> {
+    inner: Vec<&'a str>,
+}
+struct Titles<'a> {
+    inner: Vec<&'a str>,
+}
+
+fn main() {
+    let data: Vec<_> = MOCK_DATA.split('\n').skip(1).collect();
+    let names: Vec<_> = data
+        .iter()
+        .filter_map(|line| line.split(',').nth(1))
+        .collect();
+
+    let names = Names { inner: names };
+
+    let titles: Vec<_> = data
+        .iter()
+        .filter_map(|line| line.split(',').nth(4))
+        .collect();
+
+    let titles = Titles { inner: titles };
+    let data = names.inner.iter().zip(titles.inner.iter());
+    for (names, titles) in data.take(8) {
+        println!("Name: {:?}, Title: {:?}", names, titles)
+    }
+}
